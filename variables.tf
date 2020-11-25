c9c575a8b4d3c57d3f1636a63a11eb6d4e84a983
Aruna Kumar Chunduru <achunduru@advancement.local>
2020-11-24 16:05:06 -0800

    ECR

---
+++ variables.tf
@@ -0,0 +1,72 @@
+# General vars
+variable "name" {
+  description = "Name of the repository."
+  type        = string
+}
+
+variable "image_tag_mutability" {
+  description = "The tag mutability setting for the repository. Must be one of: `MUTABLE` or `IMMUTABLE`."
+  type        = string
+  default     = "MUTABLE"
+}
+
+# Image scanning configuration
+variable "image_scanning_configuration" {
+  description = "Configuration block that defines image scanning configuration for the repository. By default, image scanning must be manually triggered. See the ECR User Guide for more information about image scanning."
+  type        = map
+  default     = {}
+}
+
+variable "scan_on_push" {
+  description = "Indicates whether images are scanned after being pushed to the repository (true) or not scanned (false)."
+  type        = bool
+  default     = true
+}
+
+# Timeouts
+variable "timeouts" {
+  description = "Timeouts map."
+  type        = map
+  default     = {}
+}
+
+variable "timeouts_delete" {
+  description = "How long to wait for a repository to be deleted."
+  type        = string
+  default     = null
+}
+
+# Policy
+variable "policy" {
+  description = "Manages the ECR repository policy"
+  type        = string
+  default     = null
+}
+
+# Lifecycle policy
+variable "lifecycle_policy" {
+  description = "Manages the ECR repository lifecycle policy"
+  type        = string
+  default     = null
+}
+
+# Tags
+variable "tags" {
+  description = "A mapping of tags to assign to the resource."
+  type        = map(string)
+  default     = {}
+}
+
+# Encryption type
+variable "encryption_type" {
+  description = "The encryption type to use for the repository. Valid values are `AES256` or `KMS`"
+  type        = string
+  default     = "AES256"
+}
+
+# KMS key
+variable "kms_key" {
+  description = "The ARN of the KMS key to use when encryption_type is `KMS`. If not specified when encryption_type is `KMS`, uses a new KMS key. Otherwise, uses the default AWS managed key for ECR."
+  type        = string
+  default     = null
+}
